7d52fee842de62c98baef08a81133c270418976d
xaxexiiirl <irene.leyte@msugensan.edu.ph>
2022-01-14 03:27:32 +0800

    pls check no. 62

--- Aproject.py
+++ Aproject.py
@@ -1,9 +1,25 @@
 # import packages
 import sys
+from typing import List
+
 sys.path.append('C:\Python36\Lib\site-packages\cv2')
 import numpy as np
 import cv2
 import math
+
+# load the image
+first_read_image = cv2.imread("res.jpg")
+
+#convert image from BGR to RGB
+image = cv2.cvtColor(first_read_image, cv2.COLOR_BGR2RGB)
+
+
+rows,columns,channels = image.shape
+#rows,columns,channels
+
+#print the shape of image
+print ('Image shape: ',image.shape)
+
 
 #get mean color value of the image
 mean_color = cv2.mean(image)
@@ -11,7 +27,7 @@ mean_color = cv2.mean(image)
 print ('mean: ',mean_color)
 
 #make 3 new arrays with size equal to number of columns in small image
-numbers_of_dark_pixels_in_each_column = [0 for i in range(columns)]
+numbers_of_dark_pixels_in_each_column: list[int] = [0 for i in range(columns)]
 addition_of_row_numbers_having_dark_pixels_in_each_column = [0 for i in range(columns)]
 average_of_row_numbers_having_dark_pixels_in_each_column = [0 for i in range(columns)]
 
@@ -35,6 +51,14 @@ for x in range(0,columns):
             num += 1
             addition_of_row_numbers_having_dark_pixels_in_each_column[x] += y
 
+    # once the iteration of rows along a column is over,
+    # if num is greater than 2, set value in nums at index equal to column number, as num
+    # and set value in avgy at index equal to column number, as (value already there divided by num)
+    if num > 2:
+        numbers_of_dark_pixels_in_each_column[x] = num
+        average_of_row_numbers_having_dark_pixels_in_each_column[x] = (addition_of_row_numbers_having_dark_pixels_in_each_column[x]) / num)
+
+
 average_dark_pixels = 0
 count = 0
 for x in range(0, columns):
@@ -46,6 +70,36 @@ for x in range(0, columns):
 #gettin row and column values to extract out part of image and draw rectangle
 x_min = -1
 x_max = 0
+
+if count > 0:  # if there are values greater than 0 in nums array
+    average_dark_pixels /= count  # get the average times the num has been greater than 0 by dividing total by number of values greater than 0
+    for x in range(0, columns):
+        if numbers_of_dark_pixels_in_each_column[x] > average_dark_pixels + 15:  # if nums(column number) is greater than ((average times the num has been greater than 0) + 15)
+            if x_min < 0:  # this will happen only in first iteration as minx is -1 and after that minx is updated to x
+                x_min = x  # set minx as respective index of nums array
+        x_max = x  # set maxx as respective index of nums array
+
+if x_min >= 0:  # if minx is greater than or equal to 0 (ie. respective index of nums array saved above is higher than 0)
+    y_min = average_of_row_numbers_having_dark_pixels_in_each_column[x_min]
+    y_max = average_of_row_numbers_having_dark_pixels_in_each_column[x_max]
+    x_min_2 = x_min - 20
+
+    if x_min_2 < 0:
+            x_min_2 = 0
+    if x_min_2 > len(average_of_row_numbers_having_dark_pixels_in_each_column) - 1:
+            x_min_2 = len(average_of_row_numbers_having_dark_pixels_in_each_column) - 1
+
+    x_max_2 = x_max + 20
+    if x_max_2 < 0:
+        x_max_2 = 0
+    if x_max_2 > len(average_of_row_numbers_having_dark_pixels_in_each_column) - 1:
+        x_max_2 = len(average_of_row_numbers_having_dark_pixels_in_each_column) - 1
+
+    ratio = (average_of_row_numbers_having_dark_pixels_in_each_column[x_max_2] - average_of_row_numbers_having_dark_pixels_in_each_column[x_min_2]) / ((x_max_2) - (x_min_2))
+    y_min = (int)(ratio * (x_min - x_min_2) + average_of_row_numbers_having_dark_pixels_in_each_column[x_min_2])
+    y_max = (int)(ratio * (x_max - x_max_2) + average_of_row_numbers_having_dark_pixels_in_each_column[x_max_2])
+
+    w = x_max - x_min# width of part of image to be extracted out
 
 if w > 10 and ratio < 0.7 and ratio > -0.7:
     x1 = min(x_min, x_max)
